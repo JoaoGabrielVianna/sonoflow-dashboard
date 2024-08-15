6e3a2a991ca55229b91f014e9c8960be004caef0
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2024-08-15 15:27:02 -0300

    initial commit: v.2

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App(){
+  return(
+    <div>
+      Welcome to AppPage!
+    </div>
+  )
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@utils': path.resolve(__dirname, './src/utils')
+      '@src': '/src',
     },
   },
 })
